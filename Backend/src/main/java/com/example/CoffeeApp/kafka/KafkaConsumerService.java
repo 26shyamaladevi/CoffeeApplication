b82584adc60925b7542c9e7750a3dcf931100012
26shyamaladevi <devishyamalasankar@gmail.com>
2023-09-09 17:11:49 -0500

    Implemented retry mechanism in kafkaconsumerservice

--- Backend/src/main/java/com/example/CoffeeApp/kafka/KafkaConsumerService.java
+++ Backend/src/main/java/com/example/CoffeeApp/kafka/KafkaConsumerService.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 import com.example.CoffeeApp.services.email.EmailService;
@@ -22,6 +24,8 @@ public class KafkaConsumerService {
         this.emailService = emailService;
     }
 
+    @RetryableTopic(attempts = "3", include = {
+            RuntimeException.class }, backoff = @Backoff(delay = 120000, multiplier = 2))
     @KafkaListener(id = "myId", topics = "order-notification")
     public void listen(ConsumerRecord<String, String> record) {
         logger.info("**********FROM CONSUMER*********");
@@ -29,8 +33,12 @@ public class KafkaConsumerService {
         logger.info("Key: {}", record.key());
         logger.info("Value: {}", record.value());
 
-        emailService.sendEmail(record.key(), "Order Completed",
-                record.value());
+        try {
+            emailService.sendEmail(record.key(), "Order Completed", record.value());
+        } catch (Exception e) {
+            // Handle the email sending failure by throwing a RuntimeException.
+            throw new RuntimeException("Failed to send email", e);
+        }
 
     }
 
